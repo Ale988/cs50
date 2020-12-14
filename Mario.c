b05a0cc329cc2ca85cc61abd4e32c8ef19016efb
Ale988 <58448824+Ale988@users.noreply.github.com>
2020-12-14 19:10:54 +0100

    Create Mario.c
    World 1-1
    Toward the beginning of World 1-1 in Nintendo’s Super Mario Brothers, Mario must hop over adjacent pyramids of blocks.

---
+++ Mario.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main (void)
+{
+
+int n,m;
+
+do
+{
+    n = get_int("Grandezza desiderata: ");
+}
+while (n < 1);
+m = n;
+for (int i = 0; i < n; i++)
+{
+    for (int j = m; j > 1; j--)
+    {
+        printf(" ");
+    }
+    for (int y = m-1; y < n; y++)
+    {
+        printf("#");
+    }
+    for (int z = 0; z < 5; z++)
+    {
+        printf(" ");
+    }
+    for (int y = m-1; y < n; y++)
+    {
+        printf("#");
+    }
+    printf("\n");
+    m--;
+}
+
+}
